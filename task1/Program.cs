a70b3ef361d829526730d2c495fa98bd16c395ad
koozak2o <kaksamtobro@gmail.com>
2022-09-13 11:47:38 +0500

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+if (number.Length == 5)
+{
+    Palindrom(number);
+}
+
+else
+{
+    Console.WriteLine("Ошибка! Перезапустите программу и введите пятизначное число: ");
+}
+
+void Palindrom(string number)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("Поздравляю! Это палиндром!");
+    }
+    else
+        Console.WriteLine("Это не палиндром!");
+
+}
+
